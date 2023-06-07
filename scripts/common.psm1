3cb6c8db6a699d2a149c4c9ebd453072c1339e11
Delaram Amiri <delaramamiri@microsoft.com>
2023-06-06 17:18:33 -0700

    merge scripts

--- scripts/common.psm1
+++ scripts/common.psm1
@@ -32,3 +32,30 @@ function New-Credential
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList  @($UserName, $AdminPassword)
     return $Credential
 }
+
+
+function Get-RegressionTestArtifacts-Azure
+{
+    $ArifactVersionList = @("0.9.0")
+    $RegressionTestArtifactsPath = "$pwd\regression"
+    mkdir $RegressionTestArtifactsPath
+
+    # Download regression test artifacts for each version.
+    foreach ($ArtifactVersion in $ArifactVersionList)
+    {
+        $DownloadPath = "$RegressionTestArtifactsPath\$ArtifactVersion"
+        mkdir $DownloadPath
+        $ArtifactName = "v$ArtifactVersion/Build-x64-native-only-Release.$ArtifactVersion.zip"
+        $ArtifactUrl = "https://github.com/microsoft/ebpf-for-windows/releases/download/" + $ArtifactName
+
+        $ProgressPreference = 'SilentlyContinue'
+        Invoke-WebRequest -Uri $ArtifactUrl -OutFile "$DownloadPath\artifact.zip"
+
+        Write-Log "Extracting $ArtifactName"
+        Expand-Archive -Path "$DownloadPath\artifact.zip" -DestinationPath $DownloadPath -Force
+        Expand-Archive -Path "$DownloadPath\build-Release.zip" -DestinationPath $DownloadPath -Force
+
+        Move-Item -Path "$DownloadPath\Release\cgroup_sock_addr2.sys" -Destination "$RegressionTestArtifactsPath\cgroup_sock_addr2_$ArtifactVersion.sys" -Force
+        Remove-Item -Path $DownloadPath -Force -Recurse
+    }
+}
