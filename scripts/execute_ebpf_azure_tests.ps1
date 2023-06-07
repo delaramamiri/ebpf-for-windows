7be1aed0d14357fc6ad0992f07bfdb55783cbc10
Delaram Amiri <delaramamiri@microsoft.com>
2023-06-06 18:54:24 -0700

    create script for 4 tests

---
+++ scripts/execute_ebpf_azure_tests.ps1
@@ -0,0 +1,17 @@
+﻿# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+# param([Parameter(Mandatory=$True)] [string] $WorkingDirectory,
+#         [Parameter(Mandatory=$True)] [string] $LogFileName,
+#         [parameter(Mandatory=$false)] [bool] $VerboseLogs = $false,
+#         [parameter(Mandatory=$false)] [bool] $Coverage = $false)
+Push-Location $WorkingDirectory
+param ([parameter(Mandatory=$false)][string] $LogFileName = "TestLog.log",
+       [parameter(Mandatory=$false)][string] $WorkingDirectory = $pwd.ToString())
+
+Import-Module $WorkingDirectory\common.psm1 -ArgumentList ($LogFileName) -Force -WarningAction SilentlyContinue
+Import-Module $WorkingDirectory\run_driver_tests.psm1 -ArgumentList ($WorkingDirectory, $LogFileName) -Force -WarningAction SilentlyContinue
+
+Invoke-CICDTests 2>&1 | Write-Log
+
+Pop-Location
