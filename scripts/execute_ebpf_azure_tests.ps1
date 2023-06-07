41a0987592e2cc7e5aeb52c2726f7c258e132d84
Delaram Amiri <delaramamiri@microsoft.com>
2023-06-06 18:54:24 -0700

    create script for 4 tests

---
+++ scripts/execute_ebpf_azure_tests.ps1
@@ -0,0 +1,14 @@
+﻿# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: MIT
+
+param([Parameter(Mandatory=$True)] [string] $WorkingDirectory,
+        [Parameter(Mandatory=$True)] [string] $LogFileName,
+        [parameter(Mandatory=$false)] [bool] $VerboseLogs = $false,
+        [parameter(Mandatory=$false)] [bool] $Coverage = $false)
+$WorkingDirectory = "$Env:SystemDrive\$WorkingDirectory"
+Import-Module $WorkingDirectory\common.psm1 -ArgumentList ($LogFileName) -Force -WarningAction SilentlyContinue
+Import-Module $WorkingDirectory\run_driver_tests.psm1 -ArgumentList ($WorkingDirectory, $LogFileName) -Force -WarningAction SilentlyContinue
+
+Invoke-CICDTests -VerboseLogs $VerboseLogs -Coverage $Coverage 2>&1 | Write-Log
+
+Pop-Location
